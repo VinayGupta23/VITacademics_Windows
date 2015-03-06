c0c1529707db0e35a02bdc79d0e341f07bfa377f
VinayGupta23 <vinay.23.gupta@outlook.com>
2015-03-06 14:30:46 +0530

    Added Page templates and design

---
+++ VITacademics/VITacademics.WindowsPhone/Helpers/PivotContentManager.cs
@@ -0,0 +1,154 @@
+﻿using Academics.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VITacademics.UIControls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace VITacademics.Helpers
+{
+    public class PivotContentManager
+    {
+        private enum PivotContentType
+        {
+            CourseView = 0,
+            BasicTimetableView = 1,
+            EnhancedTimetableView = 2
+        }
+
+        private readonly Pivot _pivot;
+        private readonly Dictionary<string, Style> _stylesDictionary;
+
+        private DateTime _curDate;
+        private List<PivotItem> _pivotItems;
+        private List<DateTime> _dates;
+        private PivotContentType _currentContent;
+        private Timetable _timetable;
+
+        private void SetContentHandles(PivotContentType newContent)
+        {
+            if(_currentContent == PivotContentType.EnhancedTimetableView)
+            {
+                _timetable = null;
+                _pivot.PivotItemLoading -= Pivot_PivotItemLoading;
+            }
+
+            _currentContent = newContent;
+
+            if(newContent == PivotContentType.EnhancedTimetableView)
+            {
+                _pivot.PivotItemLoading += Pivot_PivotItemLoading;
+            }
+        }
+
+        private void Pivot_PivotItemLoading(Pivot sender, PivotItemEventArgs args)
+        {
+            int curIndex = sender.SelectedIndex;
+            for (int i = curIndex + 1; i < curIndex + 4; i++)
+            {
+                _dates[i % 5] = _dates[curIndex].AddDays(i - curIndex);
+            }
+            int previousIndex = (curIndex + 4) % 5;
+            _dates[previousIndex] = _dates[curIndex].AddDays(-1);
+            for (int i = 0; i < 5; i++)
+            {
+                (sender.Items[i] as PivotItem).Header = _dates[i].ToString("ddd dd");
+            }
+
+            if (_dates[curIndex].Date == DateTime.Now.Date)
+                args.Item.Content = new CurrentDayControl();
+            else
+                args.Item.Content = new EnhancedDayControl();
+            args.Item.DataContext = _timetable.GetDayInfo(_dates[curIndex]);
+        }
+
+        /// <summary>
+        /// Constructs a wrapper that can manage content for the passed Pivot instance.
+        /// </summary>
+        /// <param name="pivot"></param>
+        /// <remarks>
+        /// If the passed pivot instance is manipulated or modified by the user, the current wrapper may become nonfunctional and must be reconstructed.
+        /// </remarks>
+        public PivotContentManager(Pivot pivot)
+        {
+            _pivot = pivot;
+            _stylesDictionary = new Dictionary<string,Style>();
+            foreach(var resource in pivot.Resources)
+            {
+                Style style = resource.Value as Style;
+                if (style != null)
+                    _stylesDictionary.Add(resource.Key as string, style);
+            }
+        }
+
+        public void RefreshViewItems(Course course)
+        {
+            bool hasAttendance = true;
+            _pivotItems = new List<PivotItem>(2);
+            _pivotItems.Add(new PivotItem());
+            _pivotItems[0].Header = "details";
+            switch(course.CourseMode)
+            {
+                case "CBL":
+                    _pivotItems[0].Style = _stylesDictionary["CBLCoursePivotItemStyle"];
+                    break;
+                case "PBL":
+                    _pivotItems[0].Style = _stylesDictionary["PBLCoursePivotItemStyle"];
+                    break;
+                case "LBC":
+                    _pivotItems[0].Style = _stylesDictionary["LBCCoursePivotItemStyle"];
+                    break;
+                case "PBC":
+                    _pivotItems[0].Style = _stylesDictionary["PBCCoursePivotItemStyle"];
+                    hasAttendance = false;
+                    break;
+            }
+            if (hasAttendance == true)
+            {
+                _pivotItems.Add(new PivotItem());
+                _pivotItems[1].Header = "attendance";
+                _pivotItems[1].Content = new CourseAttendanceControl((course as LtpCourse).Attendance);
+            }
+
+            SetContentHandles(PivotContentType.CourseView);
+            _pivot.DataContext = course;
+            _pivot.ItemsSource = _pivotItems;
+        }
+
+        public void RefreshViewItems(Timetable timetable, bool enhancedView)
+        {
+            if (enhancedView == false)
+            {
+                _pivotItems = new List<PivotItem>(7);
+                for (int i = 0; i < 7; i++)
+                {
+                    _pivotItems.Add(new PivotItem());
+                    _pivotItems[i].Header = ((DayOfWeek)i).ToString().ToLower();
+                    _pivotItems[i].Content = new BasicDayControl();
+                    _pivotItems[i].DataContext = timetable[(DayOfWeek)i];
+                }
+                SetContentHandles(PivotContentType.BasicTimetableView);
+                _pivot.ItemsSource = _pivotItems;
+            }
+            else
+            {
+                _pivotItems = new List<PivotItem>(5);
+                _dates = new List<DateTime>(5);
+                for (int i = 0; i < 5; i++)
+                {
+                    _pivotItems.Add(new PivotItem());
+                    _dates.Add(new DateTime());
+                }
+                _dates[0] = DateTime.Now;
+                SetContentHandles(PivotContentType.EnhancedTimetableView);
+                _timetable = timetable;
+                _pivot.ItemsSource = _pivotItems;
+            }
+        }
+
+    }
+}
--- VITacademics/VITacademics.WindowsPhone/MainPage.xaml.cs
+++ VITacademics/VITacademics.WindowsPhone/MainPage.xaml.cs
@@ -15,18 +15,35 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.ViewManagement;
 using Windows.UI;
+using Academics.ContentService;
+using System.Threading.Tasks;
+using VITacademics.Common;
+using VITacademics.Helpers;
 
 
 namespace VITacademics
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page, IManageable
     {
+        private StatusBar _statusBar = StatusBar.GetForCurrentView();
+        public static PivotContentManager PivotManager
+        {
+            get;
+            private set;
+        }
+
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            MainPage.PivotManager = new PivotContentManager(mainPivot);
+
+            _statusBar.BackgroundColor = (Application.Current.Resources["AlternateDarkBrush"] as SolidColorBrush).Color;
+            _statusBar.ForegroundColor = Colors.LightGray;
+            _statusBar.ShowAsync();
+
         }
 
         /// <summary>
@@ -50,10 +67,52 @@ namespace VITacademics
         {
             return null;
         }
-
         public void LoadState(Dictionary<string, object> lastState)
         {
             
+        }
+
+        private async void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppBarButton source = (sender as AppBarButton);
+
+            _statusBar.ProgressIndicator.Text = "Refreshing...";
+            _statusBar.ProgressIndicator.ProgressValue = null;
+            _statusBar.ProgressIndicator.ShowAsync();
+            source.IsEnabled = false;
+
+            StatusCode code = await Task.Run(() => UserManager.RefreshFromServerAsync());
+
+            if (code != StatusCode.Success)
+            {
+                if (UserManager.CurrentUser.CoursesMetadata == null)
+                    _statusBar.ProgressIndicator.Text = "Unable to refresh, last tried at " + DateTimeOffset.Now.ToString("H:mm");
+                else
+                    _statusBar.ProgressIndicator.Text = "Unable to refresh, last updated " + GetDisplayString(UserManager.CurrentUser.CoursesMetadata.RefreshedDate);
+                StandardMessageDialogs.GetDialog(code).ShowAsync();
+            }
+            else
+            {
+                _statusBar.ProgressIndicator.Text = "Last refreshed " + GetDisplayString(UserManager.CurrentUser.CoursesMetadata.RefreshedDate);
+            }
+
+            _statusBar.ProgressIndicator.ProgressValue = 0;
+            source.IsEnabled = true;
+        }
+
+        private string GetDisplayString(DateTimeOffset date)
+        {
+            DateTimeOffset today = DateTimeOffset.UtcNow;
+            if (date.Month == today.Month)
+            {
+                if (date.Day == today.Day)
+
+                    return "at " + date.ToLocalTime().ToString("H:mm");
+                else
+                    return "on " + date.ToLocalTime().ToString("ddd, H:mm");
+            }
+            else
+                return "on " + date.ToLocalTime().ToString("dd MMM, H:mm");
         }
     }
 }
