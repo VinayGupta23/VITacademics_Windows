09b6e3075358a0ac95adbd29733a8897f2ed08ca
VinayGupta23 <vinay.23.gupta@outlook.com>
2015-04-11 18:47:44 +0530

    Final edits for next app revision (release)

--- VITacademics/VITacademics.WindowsPhone/UIControls/MenuControl.xaml.cs
+++ VITacademics/VITacademics.WindowsPhone/UIControls/MenuControl.xaml.cs
@@ -71,7 +71,7 @@ namespace VITacademics.UIControls
             MenuItems = new List<MenuItem>();
             MenuItems.Add(new MenuItem("overview", "a summary of marks and attendance"));
             MenuItems.Add(new MenuItem("timetable", "your regular schedule of classes"));
-            MenuItems.Add(new MenuItem("daily buzz", "your schedule and reminders integrated with attendance"));
+            MenuItems.Add(new MenuItem("daily buzz", "your schedule, reminders and attendance in one place"));
 
             this.DataContext = this;
         }
